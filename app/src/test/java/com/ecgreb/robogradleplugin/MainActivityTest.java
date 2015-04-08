6821003370eb45e90b8cab952008d777593be98e
Chuck Greb <charles.greb@gmail.com>
2015-04-08 01:24:06 -0400

    Adds Robolectric
    Tests can be run via command line or in Android Studio

--- app/src/test/java/com/ecgreb/robogradleplugin/MainActivityTest.java
+++ app/src/test/java/com/ecgreb/robogradleplugin/MainActivityTest.java
@@ -2,19 +2,30 @@ package com.ecgreb.robogradleplugin;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class MainActivityTest {
     private MainActivity activity;
 
     @Before
     public void setUp() throws Exception {
-        activity = new MainActivity();
+        activity = Robolectric.setupActivity(MainActivity.class);
     }
 
     @Test
     public void shouldNotBeNull() throws Exception {
-        assertNotNull(activity);
+        assertThat(activity).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnAppName() throws Exception {
+        assertThat(activity.getString(R.string.app_name)).isEqualTo("Robo Gradle Plugin");
     }
 }
