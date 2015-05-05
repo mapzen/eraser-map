8cff030365db60daf73ff0ce7cf1c7b28d1ab4b3
Chuck Greb <charles.greb@gmail.com>
2015-05-04 22:44:21 -0400

    Adds search results pagination indicator

--- app/src/main/kotlin/com/mapzen/privatemaps/SearchResultsView.kt
+++ app/src/main/kotlin/com/mapzen/privatemaps/SearchResultsView.kt
@@ -5,17 +5,37 @@ import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.RelativeLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 
 public class SearchResultsView(context: Context, attrs: AttributeSet)
-        : RelativeLayout(context, attrs) {
+        : LinearLayout(context, attrs), ViewPager.OnPageChangeListener {
 
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         inflater.inflate(R.layout.view_search_results, this, true)
+        setOrientation(LinearLayout.VERTICAL)
     }
 
     public fun setAdapter(adapter: PagerAdapter) {
-        (findViewById(R.id.pager) as ViewPager).setAdapter(adapter)
+        val pager = findViewById(R.id.pager) as ViewPager
+        val indicator = findViewById(R.id.indicator) as TextView
+        pager.setAdapter(adapter)
+        pager.setOnPageChangeListener(this)
+        indicator.setText(getResources().getString(R.string.search_results_indicator,
+                pager.getCurrentItem() + 1, pager.getAdapter().getCount()))
+    }
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+    }
+
+    override fun onPageSelected(position: Int) {
+        val pager = findViewById(R.id.pager) as ViewPager
+        val indicator = findViewById(R.id.indicator) as TextView
+        indicator.setText(getResources().getString(R.string.search_results_indicator,
+                position + 1, pager.getAdapter().getCount()))
+    }
+
+    override fun onPageScrollStateChanged(state: Int) {
     }
 }
--- app/src/test/java/com/mapzen/privatemaps/SearchResultsViewTest.java
+++ app/src/test/java/com/mapzen/privatemaps/SearchResultsViewTest.java
@@ -8,6 +8,7 @@ import org.robolectric.annotation.Config;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.TextView;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.robolectric.RuntimeEnvironment.application;
@@ -33,6 +34,11 @@ public class SearchResultsViewTest {
     }
 
     @Test
+    public void shouldInflateIndicator() throws Exception {
+        assertThat(searchResultsView.findViewById(R.id.indicator)).isNotNull();
+    }
+
+    @Test
     public void setAdapter_shouldSetPagerAdapter() throws Exception {
         PagerAdapter adapter = new TestPagerAdapter();
         searchResultsView.setAdapter(adapter);
@@ -40,9 +46,26 @@ public class SearchResultsViewTest {
         assertThat(pager.getAdapter()).isEqualTo(adapter);
     }
 
+    @Test
+    public void setAdapter_shouldSetIndicatorText() throws Exception {
+        PagerAdapter adapter = new TestPagerAdapter();
+        searchResultsView.setAdapter(adapter);
+        TextView indicator = (TextView) searchResultsView.findViewById(R.id.indicator);
+        assertThat(indicator.getText()).isEqualTo("1 of 3 results");
+    }
+
+    @Test
+    public void onPageSelected_shouldUpdateIndicatorText() throws Exception {
+        PagerAdapter adapter = new TestPagerAdapter();
+        searchResultsView.setAdapter(adapter);
+        searchResultsView.onPageSelected(1);
+        TextView indicator = (TextView) searchResultsView.findViewById(R.id.indicator);
+        assertThat(indicator.getText()).isEqualTo("2 of 3 results");
+    }
+
     private class TestPagerAdapter extends PagerAdapter {
         @Override public int getCount() {
-            return 0;
+            return 3;
         }
 
         @Override public boolean isViewFromObject(View view, Object object) {
