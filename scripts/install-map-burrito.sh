8bdf9ce59a152c7b24c6f682781a715f1e107e88
Chuck Greb <charles.greb@gmail.com>
2015-05-20 16:25:16 -0400

    Install VTM script needs to run before map burrito (dependency)

--- scripts/install-map-burrito.sh
+++ scripts/install-map-burrito.sh
@@ -6,7 +6,7 @@
 #   install-map-burrito.sh
 
 git clone https://github.com/mapzen/map-burrito.git
-cd map-burrito
-./gradlew clean install
-cd ..
+cd map-burrito/map-burrito
+../gradlew clean install
+cd ../..
 rm -rf map-burrito
