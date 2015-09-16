ed9c950fe98e19da17ee689b8fcf3252cda1142a
Chuck Greb <charles.greb@gmail.com>
2015-09-16 11:16:43 -0400

    Adds distance units preference

--- app/src/main/kotlin/com/mapzen/erasermap/view/SettingsActivity.kt
+++ app/src/main/kotlin/com/mapzen/erasermap/view/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.mapzen.erasermap.view
 
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
 import com.mapzen.erasermap.R
 
@@ -12,10 +13,37 @@ public class SettingsActivity : HomeAsUpActivity() {
                 .commit()
     }
 
-    class SettingsFragment : PreferenceFragment() {
+    class SettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener{
         override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+            super<PreferenceFragment>.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.preferences)
+            initDistanceUnitsPref()
+        }
+
+        override fun onPreferenceChange(preference: Preference?, value: Any?): Boolean {
+            if (preference is Preference && value is String) {
+                if (getString(R.string.distance_units_key).equals(preference.getKey())) {
+                    updateDistanceUnitsPref(preference, value)
+                    return true
+                }
+            }
+
+            return false
+        }
+
+        private fun initDistanceUnitsPref() {
+            val key = getString(R.string.distance_units_key)
+            val value = getPreferenceManager().getSharedPreferences()
+                    .getString(key, getString(R.string.distance_units_default))
+
+            updateDistanceUnitsPref(findPreference(key), value)
+            findPreference(key).setOnPreferenceChangeListener(this)
+        }
+
+        private fun updateDistanceUnitsPref(preference: Preference, value: String) {
+            val index = getResources().getStringArray(R.array.distance_units_values).indexOf(value)
+            val text = getResources().getStringArray(R.array.distance_units_entries)[index]
+            preference.setSummary(text)
         }
     }
 }
--- app/src/test/kotlin/com/mapzen/erasermap/view/SettingsActivityTest.kt
+++ app/src/test/kotlin/com/mapzen/erasermap/view/SettingsActivityTest.kt
@@ -1,26 +1,54 @@
 package com.mapzen.erasermap.view
 
+import android.preference.Preference
 import com.mapzen.erasermap.BuildConfig
 import com.mapzen.erasermap.PrivateMapsTestRunner
+import com.mapzen.erasermap.R
+import com.mapzen.erasermap.view.SettingsActivity.SettingsFragment
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
+import org.robolectric.Robolectric.setupActivity
+import org.robolectric.RuntimeEnvironment.application
 import org.robolectric.annotation.Config
 
 @RunWith(PrivateMapsTestRunner::class)
 @Config(constants = BuildConfig::class, sdk=intArrayOf(21))
 public class SettingsActivityTest {
-    var settingsActivity = Robolectric.setupActivity(javaClass<SettingsActivity>())
+    val settingsActivity = setupActivity(javaClass<SettingsActivity>())
+    val settingsFragment = settingsActivity.getFragmentManager()
+            .findFragmentById(android.R.id.content) as SettingsFragment
 
-    @Test
-    public fun shouldNotBeNull() {
+    fun findPreference(keyId: Int): Preference = settingsFragment.findPreference(getString(keyId))
+
+    fun getString(resId: Int): String = application.getString(resId)
+
+    @Test fun shouldNotBeNull() {
         assertThat(settingsActivity).isNotNull()
+        assertThat(settingsFragment).isNotNull()
     }
 
-    @Test
-    public fun shouldHaveSettingsFragment() {
-        assertThat(settingsActivity.getFragmentManager().findFragmentById(android.R.id.content))
-                .isInstanceOf(javaClass<SettingsActivity.SettingsFragment>())
+    @Test fun onCreate_shouldSetDistanceUnitsSummaryDefaultValue() {
+        settingsFragment.onCreate(null)
+        assertThat(findPreference(R.string.distance_units_key).getSummary()).isEqualTo("Miles")
+    }
+
+    @Test fun onCreate_shouldSetDistanceUnitsSummaryStoredValue() {
+        settingsFragment.getPreferenceManager()
+                .getSharedPreferences()
+                .edit()
+                .putString(getString(R.string.distance_units_key), "km")
+                .commit()
+
+        settingsFragment.onCreate(null)
+        assertThat(findPreference(R.string.distance_units_key).getSummary()).isEqualTo("Kilometers")
+    }
+
+    @Test fun onPreferenceChange_shouldUpdateDistanceUnitsSummary() {
+        settingsFragment.onPreferenceChange(findPreference(R.string.distance_units_key), "mi")
+        assertThat(findPreference(R.string.distance_units_key).getSummary()).isEqualTo("Miles")
+
+        settingsFragment.onPreferenceChange(findPreference(R.string.distance_units_key), "km")
+        assertThat(findPreference(R.string.distance_units_key).getSummary()).isEqualTo("Kilometers")
     }
 }
