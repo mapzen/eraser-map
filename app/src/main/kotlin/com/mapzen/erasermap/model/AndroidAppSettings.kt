509239ac5b0159304abd5286659407695f7fb85b
Chuck Greb <charles.greb@gmail.com>
2015-09-21 23:41:42 -0400

    Implements debug prefs in AndroidAppSettings

--- app/src/main/kotlin/com/mapzen/erasermap/model/AndroidAppSettings.kt
+++ app/src/main/kotlin/com/mapzen/erasermap/model/AndroidAppSettings.kt
@@ -16,13 +16,14 @@ public class AndroidAppSettings(val application: EraserMapApplication) : AppSett
         public val KEY_MOCK_ROUTE_VALUE: String = "edittext_mock_route"
     }
 
-    private val distanceUnitsDefaultValue = application.getString(R.string.distance_units_default)
-
     private val prefs = PreferenceManager.getDefaultSharedPreferences(application)
 
+    /**
+     * Routing distance units setting.
+     */
     override var distanceUnits: Router.DistanceUnits
         get() {
-            val value = prefs.getString(KEY_DISTANCE_UNITS, distanceUnitsDefaultValue)
+            val value = prefs.getString(KEY_DISTANCE_UNITS, null)
             when (value) {
                 Router.DistanceUnits.MILES.toString() -> return Router.DistanceUnits.MILES
                 Router.DistanceUnits.KILOMETERS.toString() -> return Router.DistanceUnits.KILOMETERS
@@ -34,23 +35,56 @@ public class AndroidAppSettings(val application: EraserMapApplication) : AppSett
             prefs.edit().putString(KEY_DISTANCE_UNITS, value.toString()).commit()
         }
 
+    /**
+     * Mock location checkbox setting.
+     */
     override var isMockLocationEnabled: Boolean
-        get() = throw UnsupportedOperationException()
+        get() {
+            return prefs.getBoolean(KEY_MOCK_LOCATION_ENABLED, false)
+        }
         set(value) {
+            prefs.edit().putBoolean(KEY_MOCK_LOCATION_ENABLED, value).commit()
         }
 
+    /**
+     * Mock location (lat, lng) value setting.
+     */
     override var mockLocation: Location
-        get() = throw UnsupportedOperationException()
+        get() {
+            val default = application.getString(R.string.edittext_mock_location_default_value)
+            val values = prefs.getString(KEY_MOCK_LOCATION_VALUE, default)
+            val split = values?.split(",")
+            val location = Location("mock")
+            location.setLatitude(split?.get(0)?.toDouble() as Double)
+            location.setLongitude(split?.get(1)?.toDouble() as Double)
+            return location
+        }
         set(value) {
+            val string = value.getLatitude().toString() + ", " + value.getLongitude().toString()
+            prefs.edit().putString(KEY_MOCK_LOCATION_VALUE, string).commit()
         }
 
+    /**
+     * Mock route checkbox setting.
+     */
     override var isMockRouteEnabled: Boolean
-        get() = throw UnsupportedOperationException()
+        get() {
+            return prefs.getBoolean(KEY_MOCK_ROUTE_ENABLED, false)
+        }
         set(value) {
+            prefs.edit().putBoolean(KEY_MOCK_ROUTE_ENABLED, value).commit()
         }
 
+    /**
+     * Mock route GPX trace file setting.
+     */
     override var mockRoute: File
-        get() = throw UnsupportedOperationException()
+        get() {
+            val default = application.getString(R.string.edittext_mock_route_default_value)
+            val value = prefs.getString(KEY_MOCK_ROUTE_VALUE, default)
+            return File(application.getExternalFilesDir(null), value)
+        }
         set(value) {
+            prefs.edit().putString(KEY_MOCK_ROUTE_VALUE, value.getName()).commit()
         }
 }
--- app/src/test/kotlin/com/mapzen/erasermap/model/AndroidAppSettingsTest.kt
+++ app/src/test/kotlin/com/mapzen/erasermap/model/AndroidAppSettingsTest.kt
@@ -4,12 +4,14 @@ import android.preference.PreferenceManager
 import com.mapzen.erasermap.BuildConfig
 import com.mapzen.erasermap.EraserMapApplication
 import com.mapzen.erasermap.PrivateMapsTestRunner
+import com.mapzen.erasermap.dummy.TestHelper
 import com.mapzen.valhalla.Router
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RuntimeEnvironment.application
 import org.robolectric.annotation.Config
+import java.io.File
 
 @RunWith(PrivateMapsTestRunner::class)
 @Config(constants = BuildConfig::class, sdk = intArrayOf(21))
@@ -47,5 +49,98 @@ public class AndroidAppSettingsTest {
         val key = AndroidAppSettings.KEY_DISTANCE_UNITS
         settings.distanceUnits = Router.DistanceUnits.KILOMETERS
         assertThat(prefs.getString(key, null)).isEqualTo(Router.DistanceUnits.KILOMETERS.toString())
+    }
+
+    @Test fun isMockLocationEnabled_shouldReturnDefaultValue() {
+        assertThat(settings.isMockLocationEnabled).isFalse()
+    }
+
+    @Test fun isMockLocationEnabled_shouldReturnPreferenceValueTrue() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_ENABLED
+        prefs.edit().putBoolean(key, true).commit()
+        assertThat(settings.isMockLocationEnabled).isTrue()
+    }
+
+    @Test fun isMockLocationEnabled_shouldReturnPreferenceValueFalse() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_ENABLED
+        prefs.edit().putBoolean(key, false).commit()
+        assertThat(settings.isMockLocationEnabled).isFalse()
+    }
+
+    @Test fun isMockLocationEnabled_shouldSetPreferenceValueTrue() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_ENABLED
+        settings.isMockLocationEnabled = true
+        assertThat(prefs.getBoolean(key, false)).isTrue()
+    }
+
+    @Test fun isMockLocationEnabled_shouldSetPreferenceValueFalse() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_ENABLED
+        settings.isMockLocationEnabled = false
+        assertThat(prefs.getBoolean(key, true)).isFalse()
+    }
+
+    @Test fun mockLocation_shouldReturnDefaultValue() {
+        assertThat(settings.mockLocation.getLatitude()).isEqualTo(40.7443)
+        assertThat(settings.mockLocation.getLongitude()).isEqualTo(-73.9903)
+    }
+
+    @Test fun mockLocation_shouldReturnPreferenceValue() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_VALUE
+        prefs.edit().putString(key, "1.0, 2.0").commit()
+        assertThat(settings.mockLocation.getLatitude()).isEqualTo(1.0)
+        assertThat(settings.mockLocation.getLongitude()).isEqualTo(2.0)
+    }
+
+    @Test fun mockLocation_shouldSetPreferenceValue() {
+        val key = AndroidAppSettings.KEY_MOCK_LOCATION_VALUE
+        val location = TestHelper.getTestLocation()
+        location.setLatitude(1.0)
+        location.setLongitude(2.0)
+        settings.mockLocation = location
+        assertThat(prefs.getString(key, null)).isEqualTo("1.0, 2.0")
+    }
+
+    @Test fun isMockRouteEnabled_shouldReturnDefaultValue() {
+        assertThat(settings.isMockRouteEnabled).isFalse()
+    }
+
+    @Test fun isMockRouteEnabled_shouldReturnPreferenceValueTrue() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_ENABLED
+        prefs.edit().putBoolean(key, true).commit()
+        assertThat(settings.isMockRouteEnabled).isTrue()
+    }
+
+    @Test fun isMockRouteEnabled_shouldReturnPreferenceValueFalse() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_ENABLED
+        prefs.edit().putBoolean(key, false).commit()
+        assertThat(settings.isMockRouteEnabled).isFalse()
+    }
+
+    @Test fun isMockRouteEnabled_shouldSetPreferenceValueTrue() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_ENABLED
+        settings.isMockRouteEnabled = true
+        assertThat(prefs.getBoolean(key, false)).isTrue()
+    }
+
+    @Test fun isMockRouteEnabled_shouldSetPreferenceValueFalse() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_ENABLED
+        settings.isMockRouteEnabled = false
+        assertThat(prefs.getBoolean(key, true)).isFalse()
+    }
+
+    @Test fun mockRoute_shouldReturnDefaultValue() {
+        assertThat(settings.mockRoute).hasName("lost.gpx")
+    }
+
+    @Test fun mockRoute_shouldReturnPreferenceValue() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_VALUE
+        prefs.edit().putString(key, "acme.gpx").commit()
+        assertThat(settings.mockRoute).hasName("acme.gpx")
+    }
+
+    @Test fun mockRoute_shouldSetPreferenceValue() {
+        val key = AndroidAppSettings.KEY_MOCK_ROUTE_VALUE
+        settings.mockRoute = File("ymca.gpx")
+        assertThat(prefs.getString(key, null)).isEqualTo("ymca.gpx")
     }
 }
