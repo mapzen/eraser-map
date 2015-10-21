95eeeabb24573b1ec86972b73e397c2913fecb8d
Chuck Greb <charles.greb@gmail.com>
2015-10-21 16:34:33 -0400

    Adds TimeView to format time for display

---
+++ app/src/main/kotlin/com/mapzen/erasermap/view/TimeView.kt
@@ -0,0 +1,36 @@
+package com.mapzen.erasermap.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+import com.mapzen.erasermap.R
+
+/**
+ * Formats time for display. Examples: <1 min, 45 mins, 2 hrs 1 min, 5 hrs 27 mins.
+ */
+public class TimeView(context: Context, attrs: AttributeSet) : TextView(context, attrs) {
+    public var timeInMinutes: Int = 0
+        set (value) {
+            if (value < 1) {
+                text = context.getString(R.string.less_than_one_minute)
+            } else {
+                formatTime(value)
+            }
+        }
+
+    private fun formatTime(value: Int) {
+        val hours = value / 60
+        val minutes = value % 60
+
+        val hourText = context.resources.getQuantityString(R.plurals.hours, hours, hours)
+        val minuteText = context.resources.getQuantityString(R.plurals.minutes, minutes, minutes)
+
+        if (hours == 0) {
+            text = minuteText
+        } else if (minutes == 0) {
+            text = hourText
+        } else {
+            text = hourText + " " + minuteText
+        }
+    }
+}
