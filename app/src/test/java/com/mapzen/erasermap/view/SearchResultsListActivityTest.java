c4d0c20fc34e0e7f0c2d58161d0b5e0b6833d94f
Chuck Greb <charles.greb@gmail.com>
2015-11-30 16:04:15 -0500

    Fix display text when viewing all search result
    Implements custom `ArrayAdapter` that uses `SimpleFeature.label()` method for display text.
    
    Closes #145

--- app/src/test/java/com/mapzen/erasermap/view/SearchResultsListActivityTest.java
+++ app/src/test/java/com/mapzen/erasermap/view/SearchResultsListActivityTest.java
@@ -2,6 +2,9 @@ package com.mapzen.erasermap.view;
 
 import com.mapzen.erasermap.BuildConfig;
 import com.mapzen.erasermap.PrivateMapsTestRunner;
+import com.mapzen.erasermap.R;
+import com.mapzen.erasermap.dummy.TestHelper;
+import com.mapzen.pelias.SimpleFeature;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,8 +12,14 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 import org.robolectric.fakes.RoboMenuItem;
+import org.robolectric.util.ActivityController;
 
+import android.content.Intent;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +43,22 @@ public class SearchResultsListActivityTest {
         MenuItem menuItem = new RoboMenuItem(android.R.id.home);
         activity.onOptionsItemSelected(menuItem);
         assertThat(activity.isFinishing()).isTrue();
+    }
+
+    @Test
+    public void shouldPopulateListViewWithLabel() throws Exception {
+        ActivityController<SearchResultsListActivity> controller =
+                Robolectric.buildActivity(SearchResultsListActivity.class);
+
+        ArrayList<SimpleFeature> simpleFeatures = new ArrayList<>();
+        SimpleFeature simpleFeature = TestHelper.getTestSimpleFeature();
+        simpleFeatures.add(simpleFeature);
+        Intent intent = new Intent();
+        intent.putExtra("features", simpleFeatures);
+        activity = controller.withIntent(intent).create().get();
+
+        ListView listView = (ListView) activity.findViewById(R.id.search_results_list_view);
+        TextView textView = (TextView) listView.getAdapter().getView(0, null, null);
+        assertThat(textView.getText()).isEqualTo(simpleFeature.label());
     }
 }
