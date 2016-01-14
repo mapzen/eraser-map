aeccce9e56a266f3983157aa02c604b5e021d57b
Varun Talwar <tallytalwar@gmail.com>
2016-01-14 12:32:06 -0500

    Update tests

--- app/src/test/kotlin/com/mapzen/erasermap/view/TestMainController.kt
+++ app/src/test/kotlin/com/mapzen/erasermap/view/TestMainController.kt
@@ -24,6 +24,7 @@ public class TestMainController : MainViewController {
     public var isDirectionListVisible: Boolean = false
     public var isRoutingModeVisible: Boolean = false
     public var isCenteredOnCurrentFeature: Boolean = false
+    public var isCenteredOnTappedFeature: Boolean = false
     public var isReverseGeocodeVisible: Boolean = false
 
     override fun showSearchResults(features: List<Feature>) {
@@ -36,6 +37,10 @@ public class TestMainController : MainViewController {
 
     override fun centerOnCurrentFeature(features: List<Feature>) {
         isCenteredOnCurrentFeature = true
+    }
+
+    override fun centerOnTappedFeature(features: List<Feature>, position: Int) {
+        isCenteredOnTappedFeature = true
     }
 
     override fun hideSearchResults() {
