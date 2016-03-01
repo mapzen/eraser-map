065adc626e34cd8dda709bdfc658f2c37c37a569
Chuck Greb <charles.greb@gmail.com>
2016-03-01 09:28:24 -0500

    Release deploy refinements

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,8 @@ if [ -z ${PERFORM_RELEASE} ]
     s3cmd put app/build/outputs/apk/app-dev-debug.apk s3://android.mapzen.com/erasermap-development/$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM.apk
   else
     scripts/install-leyndo.sh
-    git clone $CONFIG_REPO
-    ./gradlew assembleProdRelease -PvectorTileApiKey=$VECTOR_TILE_API_KEY_PROD -PpeliasApiKey=$PELIAS_API_KEY_PROD -PvalhallaApiKey=$VALHALLA_API_KEY_PROD -PbuildNumber=$RELEASE_TAG -PreleaseStoreFile=$RELEASE_STORE_FILE -PreleaseStorePassword=$RELEASE_STORE_PASSWORD -PreleaseKeyAlias=$RELEASE_KEY_ALIAS -PreleaseKeyPassword=$RELEASE_KEY_PASSWORD
+    cd app && git clone $CONFIG_REPO
+    cd ..
+    ./gradlew assembleProdRelease -PvectorTileApiKey=$VECTOR_TILE_API_KEY_PROD -PpeliasApiKey=$PELIAS_API_KEY_PROD -PvalhallaApiKey=$VALHALLA_API_KEY_PROD -PbuildNumber=$RELEASE_TAG -PreleaseStoreFile=$RELEASE_STORE_FILE -PreleaseStorePassword="$RELEASE_STORE_PASSWORD" -PreleaseKeyAlias=$RELEASE_KEY_ALIAS -PreleaseKeyPassword="$RELEASE_KEY_PASSWORD"
     s3cmd put app/build/outputs/apk/app-prod-release.apk s3://android.mapzen.com/erasermap-releases/$RELEASE_TAG.apk
 fi
--- scripts/install-leyndo.sh
+++ scripts/install-leyndo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+export PATH="$PATH:$ANDROID_NDK"
 git clone git@github.com:mapzen/leyndo.git
 cd leyndo && ./install.sh
