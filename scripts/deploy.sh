c92d1407e95d7d2a7ae66946fff03702ec3c84b2
Chuck Greb <charles.greb@gmail.com>
2016-03-01 08:27:16 -0500

    Adds install-leyndo.sh to release config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ if [ -z ${PERFORM_RELEASE} ]
     ./gradlew assembleDevDebug -PmintApiKey=$MINT_API_KEY -PvectorTileApiKey=$VECTOR_TILE_API_KEY -PpeliasApiKey=$PELIAS_API_KEY -PvalhallaApiKey=$VALHALLA_API_KEY -PbuildNumber=$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM
     s3cmd put app/build/outputs/apk/app-dev-debug.apk s3://android.mapzen.com/erasermap-development/$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM.apk
   else
+    scripts/install-leyndo.sh
     git clone $CONFIG_REPO
     ./gradlew assembleProdRelease -PvectorTileApiKey=$VECTOR_TILE_API_KEY_PROD -PpeliasApiKey=$PELIAS_API_KEY_PROD -PvalhallaApiKey=$VALHALLA_API_KEY_PROD -PbuildNumber=$RELEASE_TAG -PreleaseStoreFile=$RELEASE_STORE_FILE -PreleaseStorePassword=$RELEASE_STORE_PASSWORD -PreleaseKeyAlias=$RELEASE_KEY_ALIAS -PreleaseKeyPassword=$RELEASE_KEY_PASSWORD
     s3cmd put app/build/outputs/apk/app-prod-release.apk s3://android.mapzen.com/erasermap-releases/$RELEASE_TAG.apk
---
+++ scripts/install-leyndo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:mapzen/leyndo.git
+cd leyndo && ./install.sh
