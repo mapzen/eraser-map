581eaf6304c1eadbbb8202f4ed0495a7dc67dff5
Sarah Lensing <sarah@mapzen.com>
2016-05-19 16:44:34 -0400

    Dont show current location when routing

--- app/src/main/kotlin/com/mapzen/erasermap/controller/MainActivity.kt
+++ app/src/main/kotlin/com/mapzen/erasermap/controller/MainActivity.kt
@@ -1348,7 +1348,7 @@ class MainActivity : AppCompatActivity(), MainViewController, RouteCallback,
     }
 
     override fun checkPermissionAndEnableLocation() {
-        if (permissionManager.granted) {
+        if (permissionManager.granted && !presenter.routingEnabled) {
             mapzenMap?.isMyLocationEnabled = true
         }
     }
