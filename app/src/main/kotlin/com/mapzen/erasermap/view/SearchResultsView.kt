eed5b8db19c3e9b602caf0c2ad5c5a770535c4ec
Sarah Lensing <sarahlensing@gmail.com>
2016-10-28 10:50:11 -0600

    Search list crash (#758)
    * Set SearchResultsView autocomplete list property
    
    * Remove redundant adapter initialization

--- app/src/main/kotlin/com/mapzen/erasermap/view/SearchResultsView.kt
+++ app/src/main/kotlin/com/mapzen/erasermap/view/SearchResultsView.kt
@@ -54,10 +54,10 @@ open class SearchResultsView(context: Context, attrs: AttributeSet)
             callback: MainActivity.PeliasCallback) {
 
         this.searchView = searchView
+        this.autoCompleteListView = autoCompleteListView
 
         searchView.setRecentSearchIconResourceId(R.drawable.ic_recent)
         searchView.setAutoCompleteIconResourceId(R.drawable.ic_pin_c)
-        initAutoCompleteAdapter()
         autoCompleteListView.adapter = initAutoCompleteAdapter()
         mapzenSearch.setLocationProvider(locationProvider)
         searchView.setAutoCompleteListView(autoCompleteListView)
